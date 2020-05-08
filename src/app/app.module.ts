93f9e0cb3ff9dde7429d49d123842718ee4d9893
Sean Roger Arce <sean.arce@revemy.com>
2020-05-08 22:30:37 +0800

    add bootstrap, update bootstrap for theme, add initial layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ThemeComponent } from './main/theme/theme.component';
+import { AppRouting } from './app-routing.module';
+import { LoginComponent } from './main/public/login/login.component';
+import { PublicModule } from './main/public/public.module';
+import { LayoutsModule } from './layouts/layouts.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThemeComponent,
+    LoginComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    LayoutsModule,
+    AppRouting,
+    PublicModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/layouts/layouts.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { ProtectedLayoutComponent } from './protected-layout/protected-layout.component';
+import { PublicLayoutComponent } from './public-layout/public-layout.component';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+  imports: [RouterModule],
+  declarations: [ProtectedLayoutComponent, PublicLayoutComponent],
+  exports: [ProtectedLayoutComponent, PublicLayoutComponent]
+})
+export class LayoutsModule {}
