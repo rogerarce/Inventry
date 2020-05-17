3d90fb2b7ec8d38dfdabebfd31abf0299a208c6b
Sean Arce <arceseanroger@gmail.com>
2020-05-17 19:37:34 +0800

    updated protected layout style

--- src/app/layouts/layouts.module.ts
+++ src/app/layouts/layouts.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { ProtectedLayoutComponent } from './protected-layout/protected-layout.component';
 import { PublicLayoutComponent } from './public-layout/public-layout.component';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   declarations: [ProtectedLayoutComponent, PublicLayoutComponent],
   exports: [ProtectedLayoutComponent, PublicLayoutComponent]
 })
